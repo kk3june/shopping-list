60e7ed0af4d776a0507b5123f1dd6437202e981e
3jun <34735492+kk3june@users.noreply.github.com>
2020-12-27 14:08:55 +0900

    refactoring complete

--- js/main.js
+++ js/main.js
@@ -17,34 +17,58 @@ function onAdd() {
     item.scrollIntoView({block : 'center'});
 }
 
+
+let id = 0;         // UUID
 function createItem(text) {
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
 
-    const item = document.createElement('div');
-    item.setAttribute('class', 'item');
+    itemRow.setAttribute('data-id', id);
 
-    const span = document.createElement('span');
-    span.setAttribute('class', 'item__name');
-    span.innerText = text;
+    // const item = document.createElement('div');
+    // item.setAttribute('class', 'item');
 
-    const delBtn = document.createElement('button');
-    delBtn.setAttribute('class','item__delete');
-    delBtn.innerHTML = '<i class = "fas fa-trash-alt"></i>';
-    delBtn.addEventListener('click', () => {
-        items.removeChild(itemRow);
-    });
+    // const span = document.createElement('span');
+    // span.setAttribute('class', 'item__name');
+    // span.innerText = text;
 
-    const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class', 'item__divider');
+    // const delBtn = document.createElement('button');
+    // delBtn.setAttribute('class','item__delete');
+    // delBtn.innerHTML = '<i class = "fas fa-trash-alt"></i>';
+    // delBtn.addEventListener('click', () => {
+    //     items.removeChild(itemRow);
+    // });
 
-    item.appendChild(span);
-    item.appendChild(delBtn);
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
+    // const itemDivider = document.createElement('div');
+    // itemDivider.setAttribute('class', 'item__divider');
 
+    // item.appendChild(span);
+    // item.appendChild(delBtn);
+    // itemRow.appendChild(item);
+    // itemRow.appendChild(itemDivider);
+
+    itemRow.innerHTML = `
+    <div class="item">
+        <span class="item__name">${text}</span>
+        <button class="item__delete">
+            <i class="fas fa-trash-alt" data-id=${id}></i>
+        </button>
+    </div>
+    <div class="item__divider"></div>
+    `;
+
+    id++;
     return itemRow;
 }
+
+items.addEventListener('click', event => {
+    const id = event.target.dataset.id;
+
+    if ( id ) {
+        const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+        toBeDeleted.remove();
+    }
+})
 
 addBtn.addEventListener('click',()=> {
     onAdd();
